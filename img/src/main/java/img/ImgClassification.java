fe182be3e08e2f9e3b3e273d9722e236462fb4b5
LDL <wsldl123292@163.com>
2017-09-10 22:03:23 +0800

    修改

--- img/src/main/java/img/ImgClassification.java
+++ img/src/main/java/img/ImgClassification.java
@@ -27,6 +27,8 @@ public class ImgClassification {
 
     private static final Long MAX_SIZE = 1048576L;
 
+    private static Integer count = 0;
+
     public static void main(String[] args) throws Exception {
         if (args == null) {
             throw new Exception("请输入参数");
@@ -37,6 +39,7 @@ public class ImgClassification {
         if (args[1] == null) {
             throw new Exception("请输入目标地址");
         }
+        long start = System.currentTimeMillis();
         newFilePath = args[1];
         File parent = new File(args[0]);
         if (parent.isDirectory()) {
@@ -53,9 +56,13 @@ public class ImgClassification {
 
         }
 
+        System.out.println("执行结束,共执行了" + count + "个,共耗时:" + (System.currentTimeMillis() - start) / 1000);
+
     }
 
     private static void imageHandler(File root) throws Exception {
+        count++;
+        System.out.println("开始执行第" + count + "个");
         if (root.isDirectory()) {
             File[] files = root.listFiles();
             if (files != null) {
@@ -70,31 +77,29 @@ public class ImgClassification {
     }
 
     private static void moveFile(String newFilePath, Location location, File file, String createDate) throws Exception {
+        if (createDate == null) {
+            createDate = "未知时间";
+        }
         if (location != null) {
             File targetFile = new File(newFilePath + "/" + location.getProvince() + "/" + location.getCity() + "/" + location.getDistrict() + "/" + createDate + "/" + file.getName());
             File newFile = new File(newFilePath + "/" + location.getProvince() + "/" + location.getCity() + "/" + location.getDistrict() + "/" + createDate);
-            if (!newFile.exists()) {
-                boolean isTrue = newFile.mkdirs();
-                if (!isTrue) {
-                    throw new Exception("创建文件夹失败!");
-                }
-
-                Files.move(file, targetFile);
-            } else {
-                Files.move(file, targetFile);
-            }
+            createFileAndMove(file, targetFile, newFile);
         } else {
             File targetFile = new File(newFilePath + "/" + createDate + "/" + file.getName());
             File newFile = new File(newFilePath + "/" + createDate);
-            if (!newFile.exists()) {
-                boolean isTrue = newFile.mkdirs();
-                if (!isTrue) {
-                    throw new Exception("创建文件夹失败!");
-                }
-                Files.move(file, targetFile);
-            } else {
-                Files.move(file, targetFile);
+            createFileAndMove(file, targetFile, newFile);
+        }
+    }
+
+    private static void createFileAndMove(File file, File targetFile, File newFile) throws Exception {
+        if (!newFile.exists()) {
+            boolean isTrue = newFile.mkdirs();
+            if (!isTrue) {
+                throw new Exception("创建文件夹失败!");
             }
+            Files.move(file, targetFile);
+        } else {
+            Files.move(file, targetFile);
         }
     }
 
@@ -149,7 +154,8 @@ public class ImgClassification {
         if (latitude == null || longitude == null) {
             return null;
         }
-        BufferedReader in = null;
+        long start = System.currentTimeMillis();
+        BufferedReader in;
         URL tirc = new URL("http://api.map.baidu.com/geocoder?location=" + latitude + "," + longitude + "&output=json&key=" + "E4805d16520de693a3fe707cdc962045");
         try {
             in = new BufferedReader(new InputStreamReader(tirc.openStream(), "UTF-8"));
@@ -161,13 +167,13 @@ public class ImgClassification {
             JSONObject object = JSON.parseObject(sb.toString());
             if (object.get("status") != null && Objects.equals(object.getString("status"), "OK")) {
                 JSONObject result = JSON.parseObject(object.getString("result"));
+                System.out.println(Thread.currentThread() + "调用API获取位置完成,耗时:" + (System.currentTimeMillis() - start) / 1000);
                 return JSON.parseObject(result.getString("addressComponent"), Location.class);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
