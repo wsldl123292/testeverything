0d0eb2adecad37be91a3abcb9d78257753e0c208
liudelin <liudelin@hetrone.com>
2017-11-09 18:21:28 +0800

    mvc

---
+++ uframework/src/main/java/org/smart4j/framework/mvc/DispatcherServlet.java
@@ -0,0 +1,9 @@
+package org.smart4j.framework.mvc;
+
+/**
+ * @描述
+ * @作者 liudelin
+ * @日期 2017/11/9 16:15
+ */
+public class DispatcherServlet {
+}
---
+++ uframework/src/main/java/org/smart4j/framework/mvc/bean/View.java
@@ -0,0 +1,9 @@
+package org.smart4j.framework.mvc.bean;
+
+/**
+ * @描述
+ * @作者 liudelin
+ * @日期 2017/11/9 16:03
+ */
+public class View {
+}
