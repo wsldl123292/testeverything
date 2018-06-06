7a56c67fa0ae54ce0f2eb16b34087d9215b9d210
liudelin <liudelin@hetrone.com>
2018-06-06 18:31:58 +0800

    修改

--- basis/src/main/java/leetcode/array/Solution.java
+++ basis/src/main/java/leetcode/array/Solution.java
@@ -4,5 +4,67 @@ package leetcode.array;
  * @author liudelin
  * @date 2018/6/6 15:54
  */
-public class Main {
+public class Solution {
+
+    /**
+     * 从排序数组中删除重复项
+     * <p>
+     * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+     * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+     * 你不需要考虑数组中超出新长度后面的元素。
+     *
+     * @param nums
+     * @return
+     */
+    private static void removeDuplicates(int[] nums) {
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[j] != nums[i]) {
+                nums[++j] = nums[i];
+            }
+        }
+        for (int i = 0; i < j + 1; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+
+    /**
+     * 买卖股票的最佳时机 II
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+     * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+     * <p>
+     * 输入: [7,1,5,3,6,4]
+     * 输出: 7
+     * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+     *
+     * @param prices
+     */
+    private static void maxProfit(int[] prices) {
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (max < (prices[j] - prices[i])) {
+                    max = prices[j] - prices[i];
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+
+
+    public static void main(String[] args) {
+        /*int[] removeDuplicatesNums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        removeDuplicates(removeDuplicatesNums);*/
+
+        int[] maxProfitNums = new int[]{7, 6, 4, 3, 1};
+        maxProfit(maxProfitNums);
+
+
+    }
 }
